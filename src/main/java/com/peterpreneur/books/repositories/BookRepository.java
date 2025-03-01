7aa4fbfa08688152912519f7c2a5c9cb2b710007
Peter Setiawan <peter@Peters-MacBook-Air-2.local>
2025-02-28 23:14:21 -0600

    Initial commit

---
+++ src/main/java/com/peterpreneur/books/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.peterpreneur.books.repositories;
+
+import com.peterpreneur.books.domain.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, String> {
+}
